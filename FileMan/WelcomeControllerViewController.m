c4de3e8471ff4c3b8d0ecaa02a1aa3a40fe9a5fb
Sami Sharaf <sami.sharaf.1998@gmail.com>
2017-05-23 18:34:38 +0300

    UI Design Changes Including a New App Icon

--- FileMan/WelcomeControllerViewController.m
+++ FileMan/WelcomeControllerViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent animated:YES];
+    //[[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent animated:YES];
     
 }
 
